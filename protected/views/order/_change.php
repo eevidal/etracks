bc6a25c52b51f870824c33b2ca539e6e34b04bda
Erica <ericavidal@gmail.com>
2014-06-30 01:25:36 -0300

    estilo de _change

--- protected/views/order/_change.php
+++ protected/views/order/_change.php
@@ -1,10 +1,10 @@
-
+<div class="panel panel-default">
 <?php
 
 $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'horizontal',
 	// 'type' => 'inline',
-	'htmlOptions' => array('class' => 'well'),
+//	'htmlOptions' => array('class' => 'well'),
 	'enableAjaxValidation'=>false,
 )); 
 
@@ -50,9 +50,11 @@ $data=$model;
 	<b><?php echo CHtml::encode($data->getAttributeLabel('status_id')); ?>:</b>
 	<?php echo CHtml::encode($data->status->name); ?>
 	<br />
-
-
+<br>
+<div class="panel panel-info">
+<div class="panel-heading">
 <h3>Nuevo estado</h3>
+
 <?php echo $form->dropDownList($model,'status_id', CHtml::listData(Status::model()->findAll(), 'id', 'name')); ?>
 <br/><br/>
 	<div class="form-actions">
@@ -63,7 +65,22 @@ $data=$model;
 			//'label'=>$model_cli->isNewRecord ? 'Create' : 'Save',
 			//'label'=>$model_equi->isNewRecord ? 'Create' : 'Save',
 			'label'=>$model->isNewRecord ? 'Crear' : 'Guardar',
-		)); ?>
+		)); ?></div>
 	</div>
 
 <?php $this->endWidget(); ?>
+</div>
+
+<div class="panel panel-danger">
+  <div class="panel-heading">
+    <h3 class="panel-title">
+	<i class='glyphicon glyphicon-exclamation-sign'></i>
+	Atención al flujo de trabajo</h3>
+  </div>
+  <div class="panel-body">
+  Nueva <i class='glyphicon glyphicon-arrow-right'></i> revisando <i class='glyphicon glyphicon-arrow-right'></i>
+  presupuestar<i class='glyphicon glyphicon-arrow-right'></i> presupuestado <i class='glyphicon glyphicon-arrow-right'></i>
+  autorizado <i class='glyphicon glyphicon-arrow-right'></i> listo<i class='glyphicon glyphicon-arrow-right'></i>entregado
+  </div>
+</div> 
+</div>
