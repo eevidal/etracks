a1b86e2614e1cc8a5347b4a96259605eae4d17a4
Erica <vidal@cifasis-conicet.gov.ar>
2018-10-02 17:59:46 -0300

    ddatabase

--- raw_database/pg_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/bin/sudo -u postgres /usr/bin/pg_dump -Fc etracks > /home/jeanmco/database/prevac.gz
-/usr/bin/sudo -u postgres /usr/bin/vacuumdb --analyze etracks
-/usr/bin/sudo -u postgres /usr/bin/pg_dump -Fc etracks > /home/jeanmco/database/postvac.gz
-SCHEMA_BACKUP="/home/jeanmco/database/$(date +%F).db.schema"
-sudo -u postgres /usr/bin/pg_dump -C  etracks > $SCHEMA_BACKUP
